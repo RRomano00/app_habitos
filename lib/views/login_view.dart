cbbf3b0f6ecd212ec97583ecc37f601fbe8a19f0
RRomano00 <rodrigo.romaninho@gmail.com>
2026-05-12 22:50:25 -0300

    feat :sparkles: making some stylish things, and scrollView

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,3 +1,5 @@
+import 'package:app_habitos/widgets/background_gradient.dart';
+import 'package:app_habitos/widgets/main_button.dart';
 import 'package:flutter/material.dart';
 
 // stful
@@ -9,8 +11,91 @@ class LoginView extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<LoginView> {
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey =  GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: backgroundGradient(),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(24),
+            child: Container(
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20)
+            ),
+              child: Column(
+                children: [
+                  Text("Bem-vindo",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text('Faça login para continuar',
+                    style: TextStyle(
+                      color: Colors.grey
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            label: Text("Email")
+                          ),
+                          validator:(email) {
+                            if (email == null || email.isEmpty){
+                              return 'Preencha o campo e-mail';
+                            }
+                            
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            label: Text("Senha")
+                          ),
+                          obscureText: true,
+                          validator:(password){
+                            if(password == null || password.isEmpty){
+                              return 'Preencha o campo senha';
+                            }
+                      
+                            return null;
+                          },
+                        )
+                      ],
+                    )
+                  ),
+                  SizedBox(
+                    child: mainButton(
+                      text: "Entrar",
+                      function: (){
+                        
+                        // Validar form
+                        if(_formKey.currentState!.validate()){
+                          print(_emailController.text);
+                          print(_passwordController.text);
+                        }
+                      }
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
